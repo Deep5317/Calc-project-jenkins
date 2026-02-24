ad0c7c7ace327b85d1c79418bd4a7e11ef029ca5
Deep5317 <deep.adak90@gmail.com>
2026-02-24 16:35:59 +0530

    done with add and sub

---
+++ CalcMain.java
@@ -0,0 +1,7 @@
+public class CalcMain {
+    public static void main(String[] args) {
+        Calculator c=new Calculator();
+        System.out.println("Addition:"+c.add(10, 20));
+        System.out.println("Subtraction:"+c.sub(10, 20));
+    }
+}
